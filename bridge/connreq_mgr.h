1784adb47c2e61a1f2ace1ed73fb174c1375e133
DESKTOP-JOGO4PM\K <k579326@163.com>
2019-06-23 00:00:14 +0800

    [mod] 定义bridge部分的数据类型

--- bridge/connreq_mgr.h
+++ bridge/connreq_mgr.h
@@ -10,4 +10,22 @@ typedef enum
 {
     REQ_Add,
     REQ_Del,
-};
+}req_type_en;
+
+typedef enum
+{
+    CLT_TYPE,
+    SVR_TYPE
+}peer_type_en;
+
+typedef struct
+{
+    peer_info_t info;
+    req_type_en req_type;
+    peer_type_en peer_type;
+}conn_req_t;
+
+
+
+
+
